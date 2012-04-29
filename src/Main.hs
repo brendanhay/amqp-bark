83a763b663a3eabaac6a101f4d951d28dc0c54e7
brendanhay <brendan.g.hay@gmail.com>
2012-04-29 10:26:48 +0200

    Testing opt parsing

--- src/Main.hs
+++ src/Main.hs
@@ -1,16 +1,46 @@
 module Main (
-  main
-  ) where
+      main
+    ) where
 
 import System.Environment (getArgs)
-import Typhon.Options
+import System.Exit        (ExitCode(..), exitWith)
+import System.Console.GetOpt
+import Data.Maybe         (fromMaybe)
 
-main :: IO ()
 main = do
     args <- getArgs
-    print $ parse args
+    let (actions, nonOpts, msgs) = getOpt RequireOrder options args
+    opts <- foldl (>>=) (return defaultOptions) actions
+    let Options { optInput  = input,
+                  optOutput = output } = opts
+    input >>= output
 
--- Read a line from stdin
+data Options = Options
+    { optInput  :: IO String
+    , optOutput :: String -> IO ()
+    }
 
--- Create routing information from env or start args
+defaultOptions :: Options
+defaultOptions = Options
+    { optInput  = getContents
+    , optOutput = putStr
+    }
+
+options :: [OptDescr (Options -> IO Options)]
+options =
+    [ Option ['v'] ["version"] (NoArg showVersion)         "show version number"
+    , Option ['i'] ["input"]   (ReqArg readInput "FILE")   "input file to read"
+    , Option ['o'] ["output"]  (ReqArg writeOutput "FILE") "output file to write"
+    ]
+
+showVersion :: a -> IO Options
+showVersion _ = do
+    putStrLn "Commandline example 0.1"
+    exitWith ExitSuccess
+
+readInput :: String -> Options -> IO Options
+readInput arg opt = return opt { optInput = readFile arg }
+
+writeOutput :: String -> Options -> IO Options
+writeOutput arg opt = return opt { optOutput = writeFile arg }
 
