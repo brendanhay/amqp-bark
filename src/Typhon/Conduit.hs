87cb86435549d55f18730e173841585281c9df4c
brendanhay <brendan.g.hay@gmail.com>
2012-05-07 10:27:04 +0200

    Splitting out conduit module and tidying up conduit composition

---
+++ src/Typhon/Conduit.hs
@@ -0,0 +1,37 @@
+module Typhon.Conduit (
+      Delimiter
+    , splitConduit
+    ) where
+
+import Data.Conduit
+import Data.Word (Word8)
+
+import qualified Data.ByteString as BS
+
+class Delimiter a where
+    split :: a -> BS.ByteString -> (BS.ByteString, BS.ByteString)
+    strip :: a -> BS.ByteString -> BS.ByteString
+
+instance Delimiter Word8 where
+    split   = BS.breakByte
+    strip _ = id
+
+instance Delimiter BS.ByteString where
+    split       = BS.breakSubstring
+    strip delim = BS.drop (BS.length delim)
+
+splitConduit :: (Delimiter d, Monad m)
+               => d
+               -> Conduit BS.ByteString m BS.ByteString
+splitConduit delim =
+    conduitState BS.empty push close
+  where
+    push state input = return $ StateProducing state' res
+      where
+        (state', res) | BS.null rest  = (buffer, [])
+                      | BS.null match = (buffer, [])
+                      | otherwise     = (strip delim rest, [match])
+          where
+            buffer        = BS.append state input
+            (match, rest) = split delim buffer
+    close state = return [state]
