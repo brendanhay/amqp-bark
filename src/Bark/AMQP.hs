40d8e2d4c886ede7755aee5454b8615c40d1a355
brendanhay <brendan.g.hay@gmail.com>
2012-05-16 14:38:07 +0200

    Adding prof flags and ignoring prof output

--- src/Bark/AMQP.hs
+++ src/Bark/AMQP.hs
@@ -10,6 +10,7 @@ import Control.Monad.IO.Class     (MonadIO, liftIO)
 import Data.ByteString.Char8      (pack, unpack)
 import Data.ByteString.Lazy.Char8 (fromChunks)
 import Data.Conduit
+import Data.Hashable
 import Data.Maybe                 (fromMaybe)
 import Data.List.Split            (splitOn)
 import Network.AMQP
@@ -19,15 +20,27 @@ import qualified Data.ByteString    as BS
 import qualified Data.HashTable.IO  as H
 import qualified Bark.Message.Types as M
 
-type Set k = H.BasicHashTable k Bool
+type HashTable k v = H.BasicHashTable k v
+
+data BindingKey = BindingKey BS.ByteString BS.ByteString deriving (Eq)
+
+instance Hashable BindingKey where
+    hash (BindingKey c s) = hash [c, s]
+
+data Binding = Binding
+    { boundExchange :: String
+    , boundQueue    :: String
+    , boundKey      :: String
+    }
+
+type BindingCache = HashTable BindingKey Binding
 
 data AMQPConn = AMQPConn
-    { amqpLocal    :: BS.ByteString
-    , amqpExchange :: BS.ByteString
+    { amqpLocal    :: String
+    , amqpExchange :: String
     , amqpConn     :: Connection
     , amqpChan     :: Channel
-    , amqpQueues   :: Set BS.ByteString
-    , amqpBindings :: Set (BS.ByteString, BS.ByteString)
+    , amqpBindings :: BindingCache
     }
 
 sinkAMQP :: MonadResource m => URI -> String -> String -> Sink M.Message m ()
@@ -51,10 +64,9 @@ connect uri hostname service = do
 
     declareExchange chan exchange
 
-    queues   <- H.new
     bindings <- H.new
 
-    return $ AMQPConn (pack hostname) (pack service) conn chan queues bindings
+    return $ AMQPConn hostname service conn chan bindings
   where
     exchange = newExchange { exchangeName = service, exchangeType = "topic", exchangeDurable = True }
 
@@ -67,49 +79,39 @@ connection uri = do
     vhost = uriPath uri
     [user, pwd] = splitOn ":" $ trim info
 
-publish :: AMQPConn -> M.Message -> IO ()
-publish conn@AMQPConn{..} msg = do
-    (exchange, _) <- declare conn msg
-    publishMsg amqpChan (unpack exchange) (unpack key) payload
-  where
-    key     = (M.publishKey msg amqpLocal)
-    payload = newMsg { msgBody = fromChunks . body $ M.msgBody msg }
-
-    body (M.Payload pay) = [pay]
-    body (M.Error err)   = [err]
-
-declare :: AMQPConn -> M.Message -> IO (BS.ByteString, BS.ByteString)
-declare conn@AMQPConn{..} msg = do
-    _ <- ensureQueue conn queue
-    _ <- ensureBound conn queue key
-    return (amqpExchange, queue)
-  where
-    queue = M.queue msg amqpExchange
-    key   = M.bindKey msg
-
-ensureQueue :: AMQPConn -> BS.ByteString -> IO ()
-ensureQueue AMQPConn{..} queue = do
-    exists <- H.lookup amqpQueues queue
-    case exists of
-        Nothing -> dec >> print inf >> ins
-        Just _  -> return ()
-  where
-    dec = declareQueue amqpChan newQueue { queueName = unpack queue, queueDurable = True }
-    ins = H.insert amqpQueues queue True
-    inf = BS.concat ["Declare Q:", queue]
-
-ensureBound :: AMQPConn -> BS.ByteString -> BS.ByteString -> IO ()
-ensureBound AMQPConn{..} queue key = do
-    exists <- H.lookup amqpBindings (queue, key)
-    case exists of
-        Nothing -> bind >> print inf >> ins
-        Just _  -> return ()
-  where
-    bind = bindQueue amqpChan (unpack queue) (unpack amqpExchange) (unpack key)
-    ins  = H.insert amqpBindings (queue, key) True
-    inf  = BS.concat ["Binding Q:", queue, ", K:", key]
-
 trim :: String -> String
 trim = f . f
   where
     f = reverse . dropWhile (== '@')
+
+publish :: AMQPConn -> M.Message -> IO ()
+publish conn@AMQPConn{..} msg = do
+    Binding{..} <- declare conn msg
+    publishMsg amqpChan boundExchange boundKey $ payload msg
+
+payload :: M.Message -> Message
+payload M.Message{..} =
+    newMsg { msgBody = fromChunks $ chunk msgBody }
+  where
+    chunk (M.Payload pay) = [pay]
+    chunk (M.Error err)   = [err]
+
+declare :: AMQPConn -> M.Message -> IO Binding
+declare conn@AMQPConn{..} msg@M.Message{..} = do
+    exists <- H.lookup amqpBindings idx
+    case exists of
+        Just b  -> return b
+        Nothing -> bind conn msg idx
+  where
+    idx = BindingKey msgCategory msgSeverity
+
+bind :: AMQPConn -> M.Message -> BindingKey -> IO Binding
+bind AMQPConn{..} msg idx = do
+    _ <- declareQueue amqpChan newQueue { queueName = queue, queueDurable = True }
+    _ <- bindQueue amqpChan queue amqpExchange key
+    H.insert amqpBindings idx res
+    return res
+  where
+    queue = unpack $ M.queue msg $ pack amqpExchange
+    key   = unpack $ M.bindKey msg
+    res   = Binding amqpExchange queue key
